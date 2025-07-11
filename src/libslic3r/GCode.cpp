53c98f2eb02ff0895d2b2b329041e3d11340a2f1
Tim Schneider <tim@schneider.engineering>
2025-07-11 21:25:09 +0200

    Fix cross solid check for inside extrusion

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -5074,7 +5074,7 @@ std::string GCodeGenerator::extrude_loop(const ExtrusionLoop &original_loop, con
         inside_dist = compute_inside_distance_start(building_paths, is_hole_loop, is_full_loop_ccw,
                                                    nozzle_diam, setting_max_depth, &inside_point);
         coordf_t threshold = coordf_t(scale_t(setting_max_depth)) / 4;
-        bool cross_solid = !m_layer_solid_surfaces.empty() && contains(m_layer_solid_surfaces, inside_point);
+        bool cross_solid = !m_layer_solid_surfaces.empty() && Geometry::contains(m_layer_solid_surfaces, inside_point);
         if (inside_dist >= threshold && !cross_solid)
             m_apply_inside_layer = true;
         apply_inside = m_apply_inside_layer && !cross_solid;
